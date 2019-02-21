fafd0982b02c068be7b1ded6176c5a9a0e3337ac
Andrew Hemminger <arhemminger@gmail.com>
2019-02-20 20:59:46 -0600

    adding week-1 folder

---
+++ week-1/hemminger-discussionBoard-1.1.js
@@ -0,0 +1,11 @@
+function Employee (first, last, title) {
+
+  // property and method definitions
+
+}
+
+var employee1 = new Employee('Nikola', 'Tesla', 'Developer');
+
+//Employee.prototype.record = this.first + ' ' + this.last + ':' + this.title;
+
+//console.log (Employee.record('Ben', 'Franklin', 'QA Analyst'));
